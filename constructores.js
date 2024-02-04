1470b947c3a564d11e2b4b80b97fc37d0fda810e
ramongonzalez101 <ramongonzalez101@gmail.com>
2024-02-04 15:28:25 +0100

    clases2

--- constructores.js
+++ constructores.js
@@ -4,10 +4,24 @@ let disco1 = {
     year: 2020
 }
 
+console.log(disco1);
+
 let disco2 = {
     artist: 'charlie zaa',
     album: 'anoranzas',
     year: 2000
 }
 
+console.log(disco2);
+
 // funcion constructor
+
+function Disco(artist,album,year){
+    this.artist = artist;
+    this.album = album;
+    this.year = year; 
+}
+
+let disco3 = new Disco('jorge guerrero', 'sabana', 2010);
+
+console.log (disco3);
